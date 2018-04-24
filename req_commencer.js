e4f16de0229ea49fcf6560e6ec8e6b277c5a4679
MBA <MBA@intechS1.fr>
2018-04-24 11:20:30 +0200

    fichier de base

---
+++ req_commencer.js
@@ -0,0 +1,30 @@
+//=========================================================================
+// Traitement de "req_commencer"
+// Version : 24/04/2018
+//=========================================================================
+"use strict";
+
+var fs = require("fs");
+require('remedial');
+
+var trait = function (req, res, query) {
+
+    var marqueurs;
+    var page;
+
+    // AFFICHAGE DE LA PAGE D'ACCUEIL
+
+    page = fs.readFileSync('modele_accueil.html', 'utf-8');
+
+    marqueurs = {};
+    marqueurs.erreur = "";
+    marqueurs.compte = "";
+    page = page.supplant(marqueurs);
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(page);
+    res.end();
+};
+//--------------------------------------------------------------------------
+
+module.exports = trait;
---
+++ req_erreur.js
@@ -0,0 +1,25 @@
+//=========================================================================
+// Affichage d'une page d'erreur
+// Version : 24/04/2018
+//=========================================================================
+
+"use strict";
+
+var fs = require("fs");
+var path = require("path");
+"use strict";
+
+
+var show_erreur = function (req, res, query) {
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    res.write('ERREUR SERVEUR');
+
+    res.end();
+};
+
+//--------------------------------------------------------------------------
+
+module.exports = show_erreur;
+
