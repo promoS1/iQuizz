930bd0744b650f01d62f3ef1e612e491e05499b7
SOWA <SOWA@intechS1.fr>
2018-04-26 09:43:41 +0200

    requete jouer solo

--- req_jouer_solo.js
+++ req_jouer_solo.js
@@ -1,8 +1,6 @@
 
 
 
-
-
 "use strict";
 
 var fs = require("fs");
@@ -13,18 +11,35 @@ var trait = function (req, res, query) {
 	var page;
 	var chaine;
 	var quest;
-
+	var compte;
+	var pseudo;
+	var theme;
+	var question = [];
+	var noq;
+	var resultat;
+	
 //CREATION DU JSON JOUEUR ET AFFICHAGE DES QUESTIONS 
+	
+//	pseudo = {compte};
+	page = fs.readFileSync('question_solo_sport.html', 'utf-8');
+	chaine = fs.readFileSync("question_" + query.theme + ".json","utf-8");
+	question = JSON.parse(chaine);
 
-	quest = JSON.parse(fs.readFileSync("questions" + query.theme + ".json","utf-8"));
-	fs.writeFileSync("questions" + query.pseudo + ".json",JSON.stringify(quest),"utf-8");
+	fs.writeFileSync( query.compte + ".json",JSON.stringify(quest),"utf-8");
 	marqueurs = {};
 	marqueurs.compte = "";
-	marqueurs.q_1 = q[0];
-	marqueurs.r1 = q[1];
-	marqueurs.r2 = q[2];
-	marqueurs.r3 = q[3];
+	question.theme = query.theme;
+//	questions = [];
+//	noq = [];
+//	resultat = [];
+	
+/*********
+	marqueurs.q_1 = quest[0];
+	marqueurs.r1 = quest[1];
+	marqueurs.r2 = quest[2];
+	marqueurs.r3 = quest[3];
 	page = page.supplant(marqueurs);
+*********/
 
 res.writeHead(200, {'Content-Type': 'text/html'});
 res.write(page);
