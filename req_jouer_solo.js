8f29fd6ba5c347828700dc271b7e2731104e40a2
MBA <MBA@intechS1.fr>
2018-04-26 12:07:25 +0200

    modifications req

--- req_jouer_solo.js
+++ req_jouer_solo.js
@@ -1,34 +1,29 @@
-
-
-
-
-
+//===========================================================
+//afficher 1ere question
+//version 24/04/2018
+//=================================
 "use strict";
 
 var fs = require("fs");
 require('remedial');
 
 var trait = function (req, res, query) {
-	var marqueurs;
-	var page;
-	var chaine;
-	var quest;
 
-//CREATION DU JSON JOUEUR ET AFFICHAGE DES QUESTIONS 
+    var marqueurs;
+    var page;
 
-	quest = JSON.parse(fs.readFileSync("questions" + query.theme + ".json","utf-8"));
-	fs.writeFileSync("questions" + query.pseudo + ".json",JSON.stringify(quest),"utf-8");
-	marqueurs = {};
-	marqueurs.compte = "";
-	marqueurs.q_1 = q[0];
-	marqueurs.r1 = q[1];
-	marqueurs.r2 = q[2];
-	marqueurs.r3 = q[3];
-	page = page.supplant(marqueurs);
+    // AFFICHAGE DE LA 1ere question
 
-res.writeHead(200, {'Content-Type': 'text/html'});
-res.write(page);
-res.end();
+    page = fs.readFileSync('sport1.html', 'utf-8');
+    marqueurs = {};
+    marqueurs.erreur = "";
+    marqueurs.compte = "";
+    page = page.supplant(marqueurs);
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(page);
+    res.end();
 };
+//--------------------------------------------------------------------------
 
 module.exports = trait;
