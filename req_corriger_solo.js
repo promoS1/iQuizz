1a80fd4861484c619fe7d3fa418dd4014c4c1d86
MBA <MBA@intechS1.fr>
2018-05-16 16:25:32 +0200

    16/05/2018

--- req_corriger_solo.js
+++ req_corriger_solo.js
@@ -24,39 +24,44 @@ var trait = function (req, res, query) {
 
 	resultat = 0;
 
-	//CORRIGER LA  QUESTION , INCREMENTATION OU DECREMENTATION DU SCORE , AFFICHER LA QUESTION SUIVANTE
+	//TIRE AU SORT DU THEME CHOISI PAR LE JOUEUR
 
 	if ( query.theme === "sport" ) {
+		chaine = fs.readFileSync("questions_sport.json","utf-8");
+	} else if ( query.theme === "pub" ) {
+		chaine = fs.readFileSync("questions_pub.json","utf-8");
+	} else if ( query.theme === "cg" ) {
+		chaine = fs.readFileSync("questions_cg.json","utf-8");
+	} else if ( query.theme === "histoire" ) {
+		chaine = fs.readFileSync("questions_histoire.json","utf-8");
+	}
 
-	chaine = fs.readFileSync("questions_sport.json","utf-8");
 	questions = JSON.parse(chaine);
 	i = Number(query.no_question);
 
-		if ( query.choix === query.reponse_q ) {
+	//CORRIGE , COMMENTE LA QUESTION
+
+	if ( query.choix === questions.bonne_reponse ){
 		page = fs.readFileSync('modele_correction_solo.html', 'utf-8');
-				marqueurs["question"] = questions[i].question;
-				marqueurs["selection"] = questions[i].proposition[query.choix];
-				marqueurs["commentaire"] = "Bravo, c'est la bonne reponse" ;
-				resultat++;
-				page = page.supplant(marqueurs);
-		} else {
-				page = fs.readFileSync('modele_correction_solo.html', 'utf-8');
-                marqueurs["question"] = questions[i].question;
-                marqueurs["selection"] = questions[i].proposition[query.choix];
-                marqueurs["commentaire"] = "Bravo, c'est la bonne reponse" ;
-				page = page.supplant(marqueurs);
-
-		};
-};
-
-	marqueurs = {};
-	marqueurs.compte = "";
-	marqueurs.theme = "";
-	page = page.supplant(marqueurs);
+		marqueurs["question"] = questions[i].question;
+		marqueurs["selection"] = questions[i].proposition[query.choix];
+		marqueurs["commentaire"] = "Bravo, c'est la bonne reponse" ;
+		resultat++;
+		page = page.supplant(marqueurs);
+	} else {
+		page = fs.readFileSync('modele_correction_solo.html', 'utf-8');
+		marqueurs["question"] = questions[i].question;
+		marqueurs["selection"] = questions[i].proposition[query.choix];
+		marqueurs["commentaire"] = "Vous avez faux, la bonne reponse est ";//+questions[i].proposition[bonne_reponse] ;
+		page = page.supplant(marqueurs);
+	};
+marqueurs = {};
+marqueurs.compte = "";
+marqueurs.theme = "";
+		page = page.supplant(marqueurs);
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write(page);
 	res.end();
 };
-
 //===========================================================
 module.exports = trait;
