6e0d5b1595053be229fc4b342352a9a11beb823a
PAPA-LATYR-FAYE <PAPA-LATYR-FAYE@intechS1.fr>
2018-05-31 18:16:19 +0200

    correction duo

---
+++ req_corriger_duo.js
@@ -0,0 +1,83 @@
+//===========================================================
+//
+//version 29/05/2018
+//=================================
+"use strict";
+
+var fs = require("fs");
+require('remedial');
+
+var trait = function (req, res, query) {
+    var marqueurs ;
+    var page;
+    var chaine;
+    var compte;
+    var theme;
+    var choix;
+    var i;
+    var questions;
+    var bonne_reponse;
+    var question;
+    var proposition;
+    var no_question;
+    var resultat;
+    var nouveau;
+    var registre;
+    var objet;
+    var j;
+
+    resultat = 0;
+
+    query.choix = Number(query.choix);
+
+    //APPEL FICHIER SUIVI JOUEUR
+
+    objet = fs.readFileSync("suivi_"+ query.compte +".json","UTF-8");
+    registre = JSON.parse(objet);a
+
+    //registre["no_question"] = [];
+
+    //CHOIX DU THÉME PAR LE JOUEUR
+
+    if ( query.theme === "sport" ) {
+        chaine = fs.readFileSync("questions_sport.json","utf-8");
+    } else if ( query.theme === "pub" ) {
+        chaine = fs.readFileSync("questions_pub.json","utf-8");
+    } else if ( query.theme === "cg" ) {
+        chaine = fs.readFileSync("questions_cg.json","utf-8");
+    } else if ( query.theme === "histoire" ) {
+        chaine = fs.readFileSync("questions_histoire.json","utf-8");
+    }
+marqueurs = [];
+    questions = JSON.parse(chaine);
+    i = Number(query.no_question);
+
+    //CORRIGE , COMMENTE LA QUESTION , MODIFIE SCORE 
+
+    page = fs.readFileSync('modele_correction_duo.html', 'utf-8');
+    marqueurs["question"] = questions[i].question;
+    marqueurs["selection"] = questions[i].proposition[query.choix];
+    if ( query.choix === questions[i].bonne_reponse ){
+        marqueurs["commentaire"] ="Vous avez selectionne :{selection}" +"<br>"+"Bravo, c'est la bonne reponse" ;
+
+    } else {
+        j = questions[i].bonne_reponse;
+        marqueurs["commentaire"] ="Vous avez selectionne :{selection}"+" <br>"+"Vous avez faux, la bonne reponse est "+questions[i].proposition[j];
+
+    };
+	no_question = i;
+	registre["no_question_repondu"].push(push(no_question);
+    page = page.supplant(marqueurs);
+
+    objet = JSON.stringify(registre);
+    fs.writeFileSync("suivi_"+ query.compte +".json", objet ,"UTF-8");
+
+    //marqueurs = {};
+    marqueurs.compte = query.compte;
+    marqueurs.theme = query.theme;
+    page = page.supplant(marqueurs);
+
+    res.write(page);
+    res.end();
+};
+//=========================================================
