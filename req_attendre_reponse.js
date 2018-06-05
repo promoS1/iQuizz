871573066f74f91b66e2acc8f309b63838074fe3
MBA <MBA@intechS1.fr>
2018-06-05 09:42:33 +0200

    duo modif 05/06/2018

--- req_attendre_reponse.js
+++ req_attendre_reponse.js
@@ -7,15 +7,21 @@ require = ("remedial");
 
 var trait = function (req, res, query) {
 
-    var contenu_fichier;
     var contenu_fichier1;
+    var liste;
+    var chaines;
     var partie;
     var liste_membres;
     var adversaire;
     var i;
     var j;
+    var x;
     var compte;
-    var etat;
+    var compteur;
+    var question;
+    var questions;
+    var proposition;
+    var hote;
     var hote;
     var page;
     var marqueurs;
@@ -28,56 +34,67 @@ var trait = function (req, res, query) {
 
     // REDIRECTION VERS PAGE HTML SI JOUEUR DÉFIÉ
 
-    for (i = 0; i < liste_membres.length; i++) {
-        if (liste_membres[i].compte === query.compte) {
+    for(i = 0; i < liste_membres.length; i++) {
+        if(liste_membres[i].compte === query.compte) {
+			x = i;
 			compte = query.compte;
             adversaire = liste_membres[i].adversaire;
-				for(j = 0; j < liste_membres.length; j++) {
-					if (liste_membres[j].compte === adversaire) {
-						if (liste_membres[j].etat === "connecté") {
-							page = fs.readFileSync('modele_refus_defie.html','utf-8');
-						} else if (liste_membres[j].etat === "joue") {
-							fs.writeFileSync("partie_"+ query.compte +"_"+ adversaire +".json", "UTF-8");
-							page = fs.readFileSync("modele_questionnaire_multi.html", "utf-8");
-						} else {
-							page = fs.readFileSync("modele_attendre_reponse.html", "utf-8");
-						}
-					}
+				if(liste_membres[i].etat === "connecté") {
+					page = fs.readFileSync('modele_refus_defie.html','utf-8');
+				} else if(liste_membres[i].etat === "joue") {
+					page = fs.readFileSync("modele_questionnaire_multi.html", "utf-8");
+				} else {
+					page = fs.readFileSync("modele_attendre_reponse.html", "utf-8");
 				}
-			}
 		}
-
-/*	if (liste_membres[i].etat === "joue") {
+	}
 
 	//LE PROCESSUS DE LA PARTIE SOLO S'EXECUTE 
 
+	if (liste_membres[x].etat === "joue") {
 
+	 // TIRE AU SORT DU THEME PAR LE JOUEUR
 
+		if ( query.theme === "sport" ) {
+			chaines = fs.readFileSync("questions_sport.json","utf-8");
+		} else if ( query.theme === "pub" ) {
+			chaines = fs.readFileSync("questions_pub.json","utf-8");
+		} else if ( query.theme === "cg" ) {
+			chaines = fs.readFileSync("questions_cg.json","utf-8");
+		} else if ( query.theme === "histoire" ) {
+			chaines = fs.readFileSync("questions_histoire.json","utf-8");
+		}
 
+	 //ON SELECTIONNE UN j ALEATOIRE
 
+		questions = JSON.parse(chaines);
+		compteur = questions.length;
+		j = (Math.floor(Math.random() * compteur));
 
+	  //ON AFFICHE LE QUESTIONNAIRE
 
+		page = fs.readFileSync("modele_questionnaire_multi.html", "utf-8");
 
+		marqueurs = {};
+		marqueurs.compte = query.compte;
+		marqueurs.theme = query.theme;
+		marqueurs.mdp = query.mdp;
+		marqueurs.adversaire = adversaire;
+		marqueurs["question"] = questions[j].question;
+		marqueurs["proposition1"] = questions[j].proposition[0];
+		marqueurs["proposition2"] = questions[j].proposition[1];
+		marqueurs["proposition3"] = questions[j].proposition[2];
+		marqueurs["numero"] = j;
 
+		page = page.supplant(marqueurs);
 
+		res.writeHead(200, {'Content-type': 'text/html'});
+		res.write(page);
+		res.end();
 
-
-}*/
-	marqueurs = {};
-    marqueurs.compte = query.compte;
-    marqueurs.theme = query.theme;
-    marqueurs.mdp = query.mdp;
-    marqueurs.adversaire = adversaire;
-    page = page.supplant(marqueurs);
-
-
-    res.writeHead(200, {'Content-type': 'text/html'});
-    res.write(page);
-    res.end();
+	};
 
 };
 //==================================================
 
 module.exports = trait;
-
-
--- req_defier_oui.js
+++ req_defier_oui.js
@@ -9,11 +9,15 @@ var trait = function (req, res, query) {
 
     var compte;
     var theme;
+    var liste = {};
     var i;
     var j;
     var compteur;
     var chaines;
-    var questions;
+    var partie;
+    var contenu;
+    var adversaire;
+	var questions;
     var proposition;
     var contenu_fichier;
     var liste_membres;
@@ -21,19 +25,29 @@ var trait = function (req, res, query) {
     var proposition2;
     var proposition3;
     var page;
+    var etat;
     var marqueurs;
 
-	//
+	//LE JOUEUR A ACCEPTE LE DEFI , ON L'ATTRIBUT L'ETAT JOUE
 
 	contenu_fichier = fs.readFileSync("salon.json" , "utf-8");
 	liste_membres = JSON.parse(contenu_fichier);
 		for (j = 0; j < liste_membres.length; j++) {
 			if(liste_membres[j].compte === query.compte) {
+				adversaire = liste_membres[j].adversaire;
 				liste_membres[j].etat = "joue" ;
-				contenu_fichier = JSON.stringify(liste_membres);
-				fs.writeFileSync("salon.json", contenu_fichier , "utf-8");
+					for (j = 0; j < liste_membres.length; j++) {
+						if (liste_membres[j].compte === adversaire) {
+							liste_membres[j].etat = "joue";
+						}	
+					}
 			}
 		}
+
+	//ECRIE DANS SALON AVEC LE NOUVEAU STATUS DES JOUEURS
+
+	contenu_fichier = JSON.stringify(liste_membres);
+	fs.writeFileSync("salon.json", contenu_fichier , "utf-8");
 
 	// TIRE AU SORT DU THEME PAR LE JOUEUR
 
@@ -47,29 +61,40 @@ var trait = function (req, res, query) {
         chaines = fs.readFileSync("questions_histoire.json","utf-8");
 	}
 
-	//
+	//ON SELECTIONNE UN i ALEATOIRE
 
     questions = JSON.parse(chaines);
     compteur = questions.length;
-    i = Number(Math.floor(Math.random() * compteur));
+    i = (Math.floor(Math.random() * compteur));
 
-	//
+	//ON ECRIT LES INFOS DE LA PARTIE DANS LE JSON COMMUN AU DEUX JOUEURS
+
+	liste.theme = query.theme;
+	liste.hote = adversaire;
+	liste.compte = query.compte;
+	liste.adversaire = adversaire;
+	liste.joueur1 = query.compte ;
+	liste.score1 = 0;//SCORE DU JOUEUR DEFIE
+	liste.joueur2 = adversaire;
+	liste.score2 = 0;//SCORE DU JOUEUR QUI DEFIE
+
+	partie = JSON.stringify(liste);
+	contenu = fs.writeFileSync("partie_"+ adversaire +"_vs_"+ query.compte +".json", partie ,"UTF-8");
+
+	//ON AFFICHE LE QUESTIONNAIRE
 
 	page = fs.readFileSync("modele_questionnaire_multi.html", "utf-8");
 
 	marqueurs = {};
 	marqueurs.compte = query.compte;
 	marqueurs.theme = query.theme;
+	marqueurs.adversaire = adversaire;
 	marqueurs["question"] = questions[i].question;
 	marqueurs["proposition1"] = questions[i].proposition[0];
 	marqueurs["proposition2"] = questions[i].proposition[1];
 	marqueurs["proposition3"] = questions[i].proposition[2];
 	marqueurs["numero"] = i;
 
-	marqueurs = {};
-    marqueurs.compte = query.compte;
-    marqueurs.theme = query.theme;
-    marqueurs.mdp = query.mdp;
     page = page.supplant(marqueurs);
 
 
