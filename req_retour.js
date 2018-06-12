7d33d70ba8844c4b4b7defc9b09a5dd634cd30d2
MBA <MBA@intechS1.fr>
2018-06-12 11:31:05 +0200

    bug attente corriger

--- req_retour.js
+++ req_retour.js
@@ -8,7 +8,7 @@ require = ("remedial");
 var trait = function (req, res, query) {
 
     var compte;
-    var i;
+    var j;
     var contenu_fichier;
     var liste_membres;
     var page;
@@ -22,17 +22,18 @@ var trait = function (req, res, query) {
 
 	page = page.supplant(marqueurs);
 
-	/*contenu_fichier = fs.readFileSync("salon.json", "utf-8");
+	contenu_fichier = fs.readFileSync("salon.json", "utf-8");
     liste_membres = JSON.parse(contenu_fichier);
 
-    for (i=0; i<liste_membres.length; i++) {
-        if(query.compte === liste_membres[i].compte) {
-            liste_membres.splice(i, 1);
-        }
+	 for (j = 0; j < liste_membres.length; j++) {
+		if(liste_membres[j].compte === query.compte) {
+			liste_membres[j].etat = "connecté" ;
+            liste_membres[j].libre = "non";
+		}
     }
 
     contenu_fichier = JSON.stringify(liste_membres);
-    fs.writeFileSync("salon.json", contenu_fichier, 'UTF-8');*/
+    fs.writeFileSync("salon.json", contenu_fichier, 'UTF-8');
 
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(page);
